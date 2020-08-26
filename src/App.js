d9b85c14fffc6d038f52096836f8c7baafd7167e
EsioFreitas <esio.gustavo@gmail.com>
2020-08-26 17:09:25 -0300

    create bloc

--- src/App.js
+++ src/App.js
@@ -1,10 +1,55 @@
 import React from 'react';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [tabuleiro, setTabuleiro] = useState();
+
   return (
-    <div>
-      <h1>Grafo 1</h1>
+    <div className='bg-light' style={{ height: '100vh' }}>
+      <div className='container'>
+        <div class='text-center'>
+          <h1 class='mb-3 pt-3'>Projeto policia e ladrão</h1>
+        </div>
+
+        <div className='row'>
+          <div className='col'>
+            <h3>1 - coloque os obstaculos</h3>
+            <div className='tabuleiro'>
+              <div className='bloco' />
+              <div className='bloco' />
+              <div className='bloco' />
+              <div className='bloco' />
+              <div className='bloco' />
+
+              <div className='bloco' />
+              <div className='bloco' />
+              <div className='bloco' />
+              <div className='bloco' />
+              <div className='bloco' />
+
+              <div className='bloco' />
+              <div className='bloco' />
+              <div className='bloco' />
+              <div className='bloco' />
+              <div className='bloco' />
+
+              <div className='bloco' />
+              <div className='bloco' />
+              <div className='bloco' />
+              <div className='bloco' />
+              <div className='bloco' />
+
+              <div className='bloco' />
+              <div className='bloco' />
+              <div className='bloco' />
+              <div className='bloco' />
+              <div className='bloco' />
+            </div>
+          </div>
+          <div className='col'>arvore</div>
+        </div>
+      </div>
     </div>
   );
 }
